14fa4e93bca704c64d7c95da53eaab6ab7a0c582
Elia Schito <perlelia@gmail.com>
2010-03-13 12:51:09 +0100

    Fixed string nibbles helper spacing issue.

--- lib/string_nibbles.rb
+++ lib/string_nibbles.rb
@@ -1,8 +1,10 @@
+require 'active_support'
+
 module StringNibbles
   def nibbles width = nil
     s = self.unpack('H*').first.scan(/(..)/).flatten
     if width
-      s.in_groups_of(width).map { |group| group.join(' ') }
+      s.in_groups_of(width, '  ').map { |group| group.join(' ') }
     else
       s.join(' ')
     end
